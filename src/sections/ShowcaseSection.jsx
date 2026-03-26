33383d420b9f8ad8d4401cdeb3e0f3f3e88f06bc
acostaayala <emmanuelayala196@gmail.com>
2026-03-26 14:20:28 -0400

    15 2026

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -53,8 +53,7 @@ const AppShowcase = () => {
             </div>
             <div className="text-content">
               <h2>
-                HI!
-                
+                Hi!
               </h2>
               <p className="text-white-50 md:text-xl">
                 Bilingual customer service and administrative professional with over three years of 
